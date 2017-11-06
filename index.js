c9b07f649767d09bd34db26a514e7ace5e82c521
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-06 18:24:06 +0530

    Remove semicolon.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) =>
-  res.send(`Hello from Express on Node running on port ${PORT}`);
+  res.send(`Hello from Express on Node running on port ${PORT}`)
 );
 app.listen(PORT, () => 
-  console.log(`Your server is running on port ${PORT}`);
+  console.log(`Your server is running on port ${PORT}`)
 );
