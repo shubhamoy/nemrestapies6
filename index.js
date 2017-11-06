f0d1976df8654586ead7571cc9c157293e24c971
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-06 22:27:57 +0530

    Use routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+routes(app);
 
 app.get('/', (req, res) =>
   res.send(`Hello from Express on Node running on port ${PORT}`)
