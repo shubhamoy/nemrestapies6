fe6b40e0f0b6116fdad0904f4a0cfe68b426f9d0
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-06 18:20:09 +0530

    Add basic express code to test

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>
+  res.send(`Hello from Express on Node running on port ${PORT}`);
+);
+app.listen(PORT, () => 
+  console.log(`Your server is running on port ${PORT}`);
+);
