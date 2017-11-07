a726796a3b9c53cd899bd40da0e3e9112c92ec24
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-07 08:16:34 +0530

    Define schema

---
+++ src/models/crmModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const ContactSchema = new Schema({
+  fname: {
+    type: String,
+    required: 'Enter your first name'
+  },
+  lname: {
+    type: String,
+    required: 'Enter your last name'
+  },
+  email: {
+    type: String,
+    required: 'Enter your email address'
+  },
+  mobile: {
+    type: Number
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  },
+  deleted_at: {
+    type: Date,
+    default: NULL
+  }
+});
