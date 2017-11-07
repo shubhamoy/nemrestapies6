2bea3602b84ab74e4d12824f89f4433fd9643b40
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-11-07 07:29:59 +0530

    Add mongoose and body-parser

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
 
+//DB Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/nemapi', {
+  useMongoClient: true
+});
+
+// Setup bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 routes(app);
 
 app.get('/', (req, res) =>
